aa4abce981875b0a811a1f7cc69a4f894bb91acb
sso-hyeon <ashash1994@naver.com>
2022-04-25 15:24:30 +0900

    이미지 수정 및 코드 수정

--- js/background.js
+++ js/background.js
@@ -1,7 +1,5 @@
-const images = ["0.jpg", "1.jpg", "2.jpg"];
-
-const chosenImage = images[Math.floor(Math.random() * images.length)];
+const chosenImage = Math.floor(Math.random() * 8);
 
 const quoteWrap = document.querySelector(".quote-wrap");
 
-quoteWrap.style.background = `url(./img/${chosenImage}) no-repeat top center / cover`;
+quoteWrap.style.background = `url(./img/${chosenImage}.jpg) no-repeat center / cover`;
